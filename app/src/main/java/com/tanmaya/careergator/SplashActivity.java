55a5c46de65acf6fcf1e173783aab61becb3b1f8
tanmaya0102 <tanmaya0102@gmail.com>
2020-04-26 23:26:31 +0530

    First Commit

---
+++ app/src/main/java/com/tanmaya/careergator/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.tanmaya.careergator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView=findViewById(R.id.imageView2);
+        textView=findViewById(R.id.tv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        imageView.startAnimation(myanim);
+        textView.startAnimation(myanim);
+        final Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        Thread timer =  new Thread(){
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+        };
+        timer.start();
+
+    }
+
+}
