84d18827cf3d5262cc585643c98bde1427247fa7
tanmaya0102 <tanmaya0102@gmail.com>
2020-04-30 02:48:06 +0530

    Added Register Page

--- app/src/main/java/com/tanmaya/careergator/LoginActivity.java
+++ app/src/main/java/com/tanmaya/careergator/LoginActivity.java
@@ -1,14 +1,48 @@
 package com.tanmaya.careergator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mEmail, mPassword;
+    private Button LoginS, LoginT;
+    private TextView register;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mEmail = findViewById(R.id.input_email);
+        mPassword = findViewById(R.id.input_password);
+        LoginS = findViewById(R.id.btn_student);
+        LoginT = findViewById(R.id.btn_teacher);
+        register = findViewById(R.id.link_signup);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,Register.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
     }
 }
